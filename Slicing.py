eb25759feec90f37dea1527d18c91a5e4e1676e3
Dinesh Chinthakuntla <dineshchinthakuntla@gmail.com>
2025-06-30 14:39:09 +0530

    slicing

---
+++ Slicing.py
@@ -0,0 +1,12 @@
+#starting value and the ending value 
+
+a="RESPECT"
+b=a[2:4:1]
+print("The number after slicing is : ",b)
+
+
+c="COLLABERATION"				
+d=c[3:5:1]
+print("The number after slicing is : ",d)
+
+
